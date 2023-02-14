32b3728107c37ec73a80eb82c1da44370ef9ee98
Douglas Berkley <douglasmberkley@gmail.com>
2023-02-14 18:28:46 +0900

    Added comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Read all
   root to: 'pets#index'
-  get '/pets/new', to: 'pets#new', as: :new_pet
-  get '/pets', to: 'pets#index', as: :pets
+  get '/pets/new', to: 'pets#new', as: :new_pet # new_pet_path
+  get '/pets', to: 'pets#index', as: :pets  # pets_path
   post '/pets', to: 'pets#create'
-  get '/pets/:id', to: 'pets#show', as: :pet
+  get '/pets/:id', to: 'pets#show', as: :pet # pet_path(@pet)
   patch '/pets/:id', to: 'pets#update'
-  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet # edit_pet(@pet)
 end
